5d6c573d43f1e7ec8fd544219eb9ce14f3e3308c
lzaragosa555459-eng <l.zaragosa.555459@umindanao.edu.ph>
2026-03-31 11:23:50 +0800

    first commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    @vite(['resources/css/app.css','resources/js/app.js'])
+    <title>Document</title>
+</head>
+<body class="bg-gray-100 flex items-center justify-center min-h-screen">
+    <h1 class="text-3x1 font-bold text-blue-600">
+        laravel 12 with Tailwind css
+    </h1>
+</body>
+</html>
